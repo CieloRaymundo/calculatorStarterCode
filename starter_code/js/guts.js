10ebf8081680cf4662cade85b4c680994c12340b
Cielo Raymundo <craymundo.student@hsfi.us>
2017-03-20 20:58:24 +0000

    Calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -6,40 +6,55 @@
 function divide(x,y)
 {
 	//Write your code below this line
+	if (y=== 0){
+    
+	return "0";
+	}
+	
+	var divided = x / y;
+	return divided;
 	
 }
 
-function multiply(x,y)
-{
-	
+function multiply(x,y){
+	var multiplied = x * y;
+	return multiplied;
 }
 
-function subtract(x,y)
-{
-	
+function subtract(x,y){
+     var subtracted = x - y ;
+     return subtracted;
 }
 
-function add(x,y)
-{
-	
+function add(x,y){
+    var added = x + y;
+    return added;
 }
 
 function power(x,y)
 {
+    var powered = Math.pow(x, y);
+    return powered;
 	
 }
 
 function square_root(x)
 {
-	
+    if (x<0){
+        alert("Undefined");
+     }
+	var sqrt = Math.sqrt(x);
+	return sqrt;
 }
 
 function logBase10(x)
 {
-	
+	var log = Math.log10(x);
+	return log
 }
 
-function generateRandomNumber()
+function generateRandomNumber(x)
 {
-	
+	var random = Math.random(x)
+	return random;
 }
